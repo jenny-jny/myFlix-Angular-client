d17ce84476e810d73426e0be26c7a0cbd8741575
jenny-jny <jenny.yu.0414@gmail.com>
2021-10-26 12:01:26 -0500

    Upon successful registration, update snackBar response to "User registration successful."

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -24,13 +24,14 @@ export class UserRegistrationFormComponent implements OnInit {
 
   // This is the function responsible for sending the form inputs to the backend
   registerUser(): void {
-    this.fetchAPIData.userRegistration(this.userData).subscribe((result) => {
+    this.fetchAPIData.userRegistration(this.userData).subscribe((response) => {
       // Logic for a successful user registration goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
-      this.snackBar.open(result, 'OK', {duration: 2000});
-      }, (result) => {
-        this.snackBar.open(result, 'OK', {duration: 2000});
-      }
-    );
+      console.log(response);
+      this.snackBar.open('User registration succesful.', 'OK', {duration: 2000});
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {duration: 2000});
+    });
   }
 }
