2ea56680de8a12eef4dec3cb3b591a9a27cd140b
jenny-jny <jenny.yu.0414@gmail.com>
2021-10-27 07:09:59 -0500

    Import MovieCardComponent and add openMoviesDialog method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -23,5 +24,9 @@ export class AppComponent {
   openUserLoginDialog(): void {
     // Assigning the dialog a width
     this.dialog.open(UserLoginFormComponent, {width: '280px'});
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {width: '500px'});
   }
 }
