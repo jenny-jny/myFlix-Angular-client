2af8944086cf6aa6c9c723e2fc250e368603378c
jenny-jny <jenny.yu.0414@gmail.com>
2021-10-27 07:14:10 -0500

    Move welcome-page component logic to corresponding file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,11 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog){}
-
-  // This is the function that will open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    // Assigning the dialog a width
-    this.dialog.open(UserRegistrationFormComponent, {width: '280px'});
-  }
-
-  // This is the function that will open the dialog when the login button is clicked  
-  openUserLoginDialog(): void {
-    // Assigning the dialog a width
-    this.dialog.open(UserLoginFormComponent, {width: '280px'});
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {width: '500px'});
-  }
 }
