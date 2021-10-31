a71b391b78239693aa083bcfd4abecd9c769fe0d
jenny-jny <jenny.yu.0414@gmail.com>
2021-10-31 15:07:15 -0500

    Import MatToolbarModule, UserProfileComponent, and NavBarComponent, add username variable, and add path to UserProfileComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -15,15 +16,20 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+const username = localStorage.getItem('username');
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
-  { path: 'movies', component: MovieCardComponent }
+  { path: 'movies', component: MovieCardComponent },
+  { path: `users/${username}`, component: UserProfileComponent}
 ];
 
 @NgModule({
@@ -32,7 +38,9 @@ const appRoutes: Routes = [
     UserRegistrationFormComponent,
     UserLoginFormComponent,
     MovieCardComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    UserProfileComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
@@ -40,6 +48,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
