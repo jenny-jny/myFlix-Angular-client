eb4b445861e35b7ebc8041267ff0b8ffc35e85df
jenny-jny <jenny.yu.0414@gmail.com>
2021-11-01 18:24:11 -0500

    Initial commit

---
+++ src/app/favorite-movies/favorite-movies.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FetchAPIDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-favorite-movies',
+  templateUrl: './favorite-movies.component.html',
+  styleUrls: ['./favorite-movies.component.scss']
+})
+
+export class FavoriteMoviesComponent implements OnInit {
+  movies: any [] = [];
+  favoriteMoviesID: any [] = [];
+  favoriteMoviesData: any [] = [];
+
+  constructor(public fetchAPIData: FetchAPIDataService) { }
+
+  ngOnInit(): void {
+    this.getMovies(),
+    this.getFavoriteMovies()
+  }
+
+  getMovies(): void {
+    this.fetchAPIData.getAllMovies().subscribe((response: any) => {
+      this.movies = response;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+
+  getFavoriteMovies(): void {
+    this.fetchAPIData.getUser().subscribe((response: any) => {
+      this.favoriteMoviesID = response.FavoriteMovies;
+      console.log(this.favoriteMoviesID);
+      this.movies.forEach((movie: any) => {
+        if(this.favoriteMoviesID.includes(movie._id)){
+          this.favoriteMoviesData.push(movie);
+        }
+      })
+      return this.favoriteMoviesData;
+    });
+  }
+
+}
