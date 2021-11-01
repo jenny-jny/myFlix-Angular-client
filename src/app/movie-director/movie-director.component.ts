7ffc86d479e46118ce014ab8261684845b86e43d
jenny-jny <jenny.yu.0414@gmail.com>
2021-11-01 13:38:21 -0500

    Initial commit

---
+++ src/app/movie-director/movie-director.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-movie-director',
+  templateUrl: './movie-director.component.html',
+  styleUrls: ['./movie-director.component.scss']
+})
+
+export class MovieDirectorComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+      public data: {
+        name: string,
+        birth: string,
+        death: string,
+        bio: string
+      }
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
