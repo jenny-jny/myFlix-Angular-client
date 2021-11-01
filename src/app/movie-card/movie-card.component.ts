2f477e18d9e9947a8f8f615f5045c1fd97059694
jenny-jny <jenny.yu.0414@gmail.com>
2021-11-01 18:35:40 -0500

    Import MatDialog, MatSnackBar, MovieGenreComponent, MovieDirectorComponent, MovieSynopsisComponent. Add addToFavorite variable. Add openMovieGenreDialog(), openMovieDirectorDialog(), openMovieSynopsisDialog(), addFavorite(), removeFavorite()

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FetchAPIDataService} from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { FetchAPIDataService } from '../fetch-api-data.service';
+import { MovieGenreComponent } from '../movie-genre/movie-genre.component';
+import { MovieDirectorComponent } from '../movie-director/movie-director.component';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,8 +15,9 @@ import {FetchAPIDataService} from '../fetch-api-data.service';
 
 export class MovieCardComponent implements OnInit {
   movies: any [] = [];
+  addToFavorite: boolean = true;
 
-  constructor(public fetchAPIData: FetchAPIDataService) { }
+  constructor(public fetchAPIData: FetchAPIDataService, public dialog: MatDialog, public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.getMovies();
@@ -21,6 +28,53 @@ export class MovieCardComponent implements OnInit {
       this.movies = response;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  openMovieGenreDialog(name: string, description: string): void {
+    this.dialog.open(MovieGenreComponent, {
+      data: {name, description}, 
+      width: '280px'
+    });
+  }
+
+  openMovieDirectorDialog(name: string, birth: string, death: string, bio: string): void {
+    this.dialog.open(MovieDirectorComponent, {
+      data: {name, birth, death, bio}, 
+      width: '280px'
+    });
+  }
+
+  openMovieSynopsisDialog(description: string): void {
+    this.dialog.open(MovieSynopsisComponent, {
+      data: {description}, 
+      width: '280px'
+    });
+  }
+
+  addFavorite(_id: string): void {
+    this.addToFavorite = false;
+    this.fetchAPIData.addFavoriteMovie(_id).subscribe((response: any) => {
+      let favoriteMovies = response.FavoriteMovies;
+      console.log(favoriteMovies);
+      this.snackBar.open('Added to favorite movies.', 'OK', {duration: 2000});
+      return favoriteMovies;
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {duration: 2000});
+    });
+  }
+
+  removeFavorite(_id: string): void {
+    this.addToFavorite = true;
+    this.fetchAPIData.deleteFavoriteMovie(_id).subscribe((response: any) => {
+      let favoriteMovies = response.FavoriteMovies;
+      console.log(favoriteMovies);
+      this.snackBar.open('Removed from favorite movies.', 'OK', {duration: 2000});
+      return favoriteMovies;
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {duration: 2000});
     });
   }
 }
